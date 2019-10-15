8579edbe5899eebe2d67459ac5228ad5b52e6544
chuthamat66 <59050796@go.buu.ac.th>
2019-10-15 17:25:46 +0700

    PP

--- send_mail.py
+++ send_mail.py
@@ -1,12 +1,12 @@
 import yagmail
 
-yag = yagmail.SMTP('your@gmail.com', 'password')
+yag = yagmail.SMTP('5900796@go.buu.ac.th', 'phutoperjer66')
 contents = """
     I have wrote this email for testing Gmail srvices!
     Yagmail is an instant service of Gmail...
 """
 yag.send(
-    to='sendToMail@live.com',
+    to='59050801@go.buu.ac.th',
     subject='TOT Today',
     contents=contents,
 )
