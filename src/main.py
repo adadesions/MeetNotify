c8274a6cc8544796b405d16dd3b4258cac775251
adadesions <adadesions@gmail.com>
2019-12-04 18:26:10 +0700

    Refactoring main file to main() and Add new filtering section

--- src/main.py
+++ src/main.py
@@ -9,47 +9,61 @@ import base64
 
 from email.parser import Parser
 
-
 SERVER = "pop.gmail.com"
 USER  = "malunthakesr@gmail.com"
 PASSWORD = "#tot#8899"
 
-# connect to server
-logging.debug('connecting to ' + SERVER)
-server = poplib.POP3_SSL(SERVER)
 
-# log in
-server.user(USER)
-server.pass_(PASSWORD)
+def main():
+    # connect to server
+    logging.debug('connecting to ' + SERVER)
+    server = poplib.POP3_SSL(SERVER)
 
-# list items on server
-resp, items, octets = server.list()
+    # log in
+    server.user(USER)
+    server.pass_(PASSWORD)
 
-# download the first message in the list
-id, size = items[-1].decode('utf-8').split(" ")
-print('=== Email Information ===')
-print("id: ", id, " size: ", size)
-resp, text, octets = server.retr(id)
+    # list items on server
+    resp, items, octets = server.list()
 
-# # convert list to Message object
-text = '\n'.join([t.decode('utf-8') for t in text])
-raw_mail = text.split('\n')
-bodyContent = emailf.getEmailContent(raw_mail)
-message = Parser().parsestr(text)
-subject = emailf.subjectHandler(message['Subject'])
-thaiTime = emailf.convert2GTM7(message['Date'])
+    # download the first message in the list
+    id, size = items[-1].decode('utf-8').split(" ")
+    print('=== Email Information ===')
+    print("id: ", id, " size: ", size)
+    resp, text, octets = server.retr(id)
 
-full_text = {
-    'from': message['From'],
-    'subject': subject,
-    'date': message['Date'],
-    'thai-time': thaiTime, 
-    'body': bodyContent
-}
+    # # convert list to Message object
+    text = '\n'.join([t.decode('utf-8') for t in text])
+    raw_mail = text.split('\n')
+    bodyContent = emailf.getEmailContent(raw_mail)
+    message = Parser().parsestr(text)
+    subject = emailf.subjectHandler(message['Subject'])
+    thaiTime = emailf.convert2GTM7(message['Date'])
 
-# Moving Email
-is_new = emailf.move_mail(USER, PASSWORD, 'Meeting')
+    # Filtering 
+    meetingFilter = ['Urgent', 'meeting', 'join', 'ประชุม', 'เรียกพบ', 'เข้าร่วม']
+    isSubjectMatch = any(f in subject for f in meetingFilter)
+    isBodyMatch = any(f in bodyContent for f in meetingFilter)
+    # End Filtering
 
-if is_new:
-    # Output message
-    print(full_text)
+    if isSubjectMatch or isBodyMatch:
+        full_text = {
+            'from': message['From'],
+            'subject': subject,
+            'date': message['Date'],
+            'thai-time': thaiTime, 
+            'body': bodyContent
+        }
+
+        # Moving Email
+        is_new = emailf.move_mail(USER, PASSWORD, 'Meeting')
+
+        if is_new:
+            # Output message
+            print(full_text)
+    else:
+        print('No match emails or No new mails in mailbox')
+
+
+if __name__ == '__main__':
+    main()
--- src/send_mail.py
+++ src/send_mail.py
@@ -21,6 +21,16 @@ data = {
             'duration: 15.00 - 17.00 pm',
             'date: 5th december 2019'
         ]
+    },
+    'out': {
+        'subject': 'This is a spam mail',
+        'body': [
+            'Dear User,',
+            'We need some help from the team about new requirement which we received from an financial user',
+            'Please visit us at ROOM 39',
+            'duration: 15.00 - 17.00 pm',
+            'date: 5th december 2019'
+        ]
     }
 }
 
@@ -40,6 +50,8 @@ def single_mail(subject, contents):
 if __name__ == '__main__':
     thai_mail = data['thai']
     eng_mail = data['eng']
+    out_mail = data['out']
 
-    # single_mail(thai_mail['subject'], thai_mail['body'])
-    single_mail(eng_mail['subject'], eng_mail['body'])
+    single_mail(thai_mail['subject'], thai_mail['body'])
+    # single_mail(eng_mail['subject'], eng_mail['body'])
+    # single_mail(out_mail['subject'], out_mail['body'])
